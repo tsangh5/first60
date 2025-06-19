5cef051f6940e2bbf1e46039bc29a3353817287e
tsangbe3 <benjamin.tsang@mail.utoronto.ca>
2025-06-19 02:24:21 -0400

    minor bug fixes [khun]

--- first60/src/app/App.tsx
+++ first60/src/app/App.tsx
@@ -1,10 +1,11 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "./components/ui/toaster";
+import { Toaster as Sonner } from "./components/ui/sonner";
+import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./page";
 import NotFound from "./NotFound";
+import 'index.css';
 
 const queryClient = new QueryClient();
 
